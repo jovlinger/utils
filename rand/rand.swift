24386b6058849aec1742a100193566b683ce4842
Johan <jovlinger@gmail.com>
2018-08-12 15:37:44 -0400

    checkpoint swift

--- rand/rand.swift
+++
@@ -1,3 +0,0 @@
-// implementation of rand in swift
-
-class
--- rand/rand/rand/main.swift
+++ rand/rand/rand/main.swift
@@ -8,9 +8,58 @@
 
 import Foundation
 
-class Inputs {
+extension Int {
+    static func random(in range: CountableRange<Int>) -> Int {
+        return range.lowerBound + Int(arc4random_uniform(UInt32(range.upperBound - range.lowerBound)))
+    }
+}
+
+struct Inputs {
     var files : [InputStream]
     init(filenames : [String]) {
-        files = filenames.map({(filename : String) in open(filename)})
+        // files = filenames.map({(filename : String) in open(filename)})
+    }
+
+    // Return next input line, or nil if all inputs are EOF.
+    mutating func line() -> String? {
+
+    }
+}
+
+struct Buffer {
+    var lines = [String]()
+    var count = 0
+    var inputs : Inputs
+    let cap : Int
+    
+    init(inputs : Inputs, cap :Int = 1000) {
+        self.inputs = inputs
+        self.cap = cap
+    }
+    // If line && full : return random existing entry, replacing with line.
+    // if line && !full : append line to buffer
+    // if !line: return random entry.
+    mutating func procline(line: String?) -> String? {
+        if let theline = line {
+            lines.append(theline)
+        }
+        var idx = Int.random(in: 0 ..< cap)
+        var l = lines[idx]
+        lines[idx] = lines[-1]
+        lines[-1] = l
+        if line == nil || lines.count > cap {
+            return lines.popLast()
+        }
+        return nil
+    }
+    
+    mutating func choose() -> String? {
+        count += 1
+        var line = inputs.line()
+        if (lines.count == cap) && (Int.random(in: 0 ..< cap) == 0) {
+            // output next line directly
+            return line
+        }
+        return procline(line: line)
     }
 }
