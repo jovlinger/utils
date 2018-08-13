0ea777dfc5a8a28b9a072fb5b627878ebcc737be
Johan <jovlinger@gmail.com>
2018-08-13 15:17:41 -0400

    checkpoint swift; compiles, but untested

--- rand/rand/rand/main.swift
+++ rand/rand/rand/main.swift
@@ -14,15 +14,74 @@ extension Int {
     }
 }
 
+struct Input {
+    let readSize = 1024
+    var filehandle : FileHandle!
+    var buffer : Data
+    let delim = "\n".data(using: .utf8)! // why is it wrong to say String.Encoding.utf8 ?
+    
+    init(filehandle : FileHandle) {
+        buffer = Data(capacity: readSize)
+        self.filehandle = filehandle
+    }
+    
+    mutating func close() {
+        if filehandle == nil {
+            return
+        }
+        filehandle.closeFile()
+        filehandle = nil
+    }
+    
+    mutating func nextLine() -> String? {
+        // Pretty much copied from https://gist.github.com/klgraham/6fe11ed1e3fe075f5ffc8b7ca350bce4
+        // nil implies EOF
+        
+        while filehandle != nil {
+            if let range = buffer.range(of: delim) {
+                // We have a delim, so clip and return upto there
+                // upperBound -> include delim, else lowerBound -> exclude
+                let line = String(data: buffer.subdata(in: 0..<range.upperBound), encoding: .utf8)
+                buffer.removeSubrange(0..<range.upperBound)
+                return line
+            }
+            // No delim, so read some more
+            let tmpData = filehandle.readData(ofLength: readSize)
+            if tmpData.count > 0 {
+                buffer.append(tmpData)
+            } else {
+                // EOF or read error.
+                if buffer.count > 0 {
+                    // Buffer contains last line in file (not terminated by delimiter).
+                    let line = String(data: buffer, encoding: .utf8)! + "\n"
+                    buffer.count = 0
+                    close()
+                    return line
+                }
+            }
+        }
+        return nil
+    }
+}
+
 struct Inputs {
-    var files : [InputStream]
-    init(filenames : [String]) {
-        // files = filenames.map({(filename : String) in open(filename)})
+    var inputs : [Input]
+    var i = 0
+    init(filehandles : [FileHandle]) {
+        inputs = filehandles.map({fh in return Input(filehandle: fh)})
     }
 
     // Return next input line, or nil if all inputs are EOF.
     mutating func line() -> String? {
-
+        while inputs.count > 0 {
+            i = (i + 1) % inputs.count
+            if let nl = inputs[i].nextLine() {
+                return nl
+            } else {
+                inputs.remove(at: i)
+            }
+        }
+        return nil
     }
 }
 
@@ -43,8 +102,8 @@ struct Buffer {
         if let theline = line {
             lines.append(theline)
         }
-        var idx = Int.random(in: 0 ..< cap)
-        var l = lines[idx]
+        let idx = Int.random(in: 0 ..< cap)
+        let l = lines[idx]
         lines[idx] = lines[-1]
         lines[-1] = l
         if line == nil || lines.count > cap {
@@ -55,7 +114,7 @@ struct Buffer {
     
     mutating func choose() -> String? {
         count += 1
-        var line = inputs.line()
+        let line = inputs.line()
         if (lines.count == cap) && (Int.random(in: 0 ..< cap) == 0) {
             // output next line directly
             return line
@@ -63,3 +122,25 @@ struct Buffer {
         return procline(line: line)
     }
 }
+
+func randomize(ins: [FileHandle], out: FileHandle) {
+    var buffer = Buffer(inputs: Inputs(filehandles: ins))
+    while let line = buffer.choose() {
+        out.write(line.data(using: .utf8)!)
+    }
+}
+
+// Make fake filehandle for testing
+func somemain() {
+    var filehandles = [FileHandle]()
+    // FIXME parse -b flag
+    for arg in CommandLine.arguments {
+       filehandles.append(FileHandle(forReadingAtPath: arg)!)
+    }
+    if filehandles.count == 0 {
+        filehandles.append(FileHandle.standardInput)
+    }
+    randomize(ins: filehandles, out: FileHandle.standardOutput)
+}
+
+
