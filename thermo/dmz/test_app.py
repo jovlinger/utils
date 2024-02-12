04818e26af88926f7e813a527597ab715f8985cb
Johan <jovlinger@gmail.com>
2024-02-12 16:26:38 -0500

    pathget needed

--- thermo/dmz/test_app.py
+++ thermo/dmz/test_app.py
@@ -20,19 +20,23 @@ class DMZTest(TestCase):
         self.assertEqual(res.status_code, 200)
         return res.get_json()
 
+    def reset(self,c, commands={}, sensors={}):
+        self.post_200(c, "/test_reset", {"commands":{}, "sensors": {}})
+
     def test_update_backendz1(self):
+        assert False
         with app.test_client() as c:
+            self.reset(c)
             # now accumulate some traffic
-            self.post_200(c, '/zone/z1/command', json={'lolidk': 'what'})
-            self.post_200(c, '/zone/z1/sensors', json={'temp_centigrade': 11.45})
+            self.post_200(c, '/zone/z1/command', {'lolidk': 'what'})
+            self.post_200(c, '/zone/z1/sensors', {'temp_centigrade': 11.45})
             self.post_200(c, '/zone/z2/sensors', 
-                         json={'temp_centigrade': 21.34, 'humid_percent': 99.99 })
-            js12 = self.post_200(c, '/zone/z1/command', json={'lolidk': 'make it so'})
-            self.post_200(c, '/zone/z3/command', json={'lolidk': 'who'})
+                         {'temp_centigrade': 21.34, 'humid_percent': 99.99 })
+            js12 = self.post_200(c, '/zone/z1/command', {'lolidk': 'make it so'})
+            self.post_200(c, '/zone/z3/command', {'lolidk': 'who'})
 
             # now we have something to read
-            js13 = self.post_200(c, '/zone/z1/sensors', 
-                         json={ 'temp_centigrade': 13.34 })
+            js13 = self.post_200(c, '/zone/z1/sensors', { 'temp_centigrade': 13.34 })
             
             self.assertNotEqual(js12['command']['last_access_dt'], js13['command']['last_access_dt'],
                                 "Expected access times to be updated on /zone/ endpoint")
@@ -44,13 +48,26 @@ class DMZTest(TestCase):
 
             # now we have something to read
             js = self.get_200(c, '/zones')
-            print(f"js : {js}")
+            print(f"js50 : {js}")            
             self.assertEqual(['z1', 'z2', 'z3'], sorted(js.keys()))
             self.assertEqual(js13, js['z1']) # including access times
             
 
-    def test_update_backendz1_something_then_clear(self):
+    def test_update_backendz1_read_last_command(self):
         with app.test_client() as c:
-            res = c.post('/zone/z1/command', json={'lolidk': 'what'})
-            res = c.post('/zone/z1/command', json={})
-            res = c.post('/zone/z1/command', json=None)
+            self.reset(c)
+            self.post_200(c, '/zone/z1/command', {'lolidk': 'what'})
+            self.post_200(c, '/zone/z1/command', {})
+            js = self.get_200(c, '/zones')
+            print(f"js57 : {js}")            
+            self.assertEqual('', pathget(js, "command.lolidk"))
+            self.post_200(c, '/zone/z1/command', {'lolidk': 'who'})
+            js = self.get_200(c, '/zones')
+            print(f"js66 : {js}")            
+            self.assertEqual('who', pathget(js, "command.lolidk"))
+
+
+    def test_update_backendz1_two_commands_then_update_from_onboard(self):
+        assert False
+        with app.test_client() as c:
+            pass
